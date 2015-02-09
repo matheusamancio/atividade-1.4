b0e724796d45cad6a6472a130248ffa92c31df8d
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-02-09 08:11:18 -0200

    pega ai evandro

---
+++ Mascote.h
@@ -0,0 +1,19 @@
+//
+//  Mascote.h
+//  programa 1.4
+//
+//  Created by Matheus Amancio Seixeiro on 2/9/15.
+//  Copyright (c) 2015 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mascote : NSObject
+{
+    int vida;
+    int energia;
+}
+- (instancetype) initWithVida : (int)v andEnergia : (int)e;
+- ()
+
+@end
---
+++ Mascote.m
@@ -0,0 +1,13 @@
+//
+//  Mascote.m
+//  programa 1.4
+//
+//  Created by Matheus Amancio Seixeiro on 2/9/15.
+//  Copyright (c) 2015 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+#import "Mascote.h"
+
+@implementation Mascote
+
+@end
